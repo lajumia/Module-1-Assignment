5c0331e823fa6836daa0944662081a09a60308ae
Md Laju Mia <developerlaju@gmail.com>
2023-09-12 01:39:39 -0700

    Assignment 1 is done in Module 1

---
+++ personal_info.php
@@ -0,0 +1,12 @@
+<?php
+$name = "Md Laju Miah";
+$age = 21;
+$country = "Bangladesh";
+$aboutMe = "I am Laju. I want to be a PHP and Laravel developer. I love PHP programming language. I hope I will 
+learn more about PHP with Ostad platform.";
+
+
+echo "My name is {$name}.</br>";
+echo "I am {$age} years old.</br>";
+echo "I live in Rangpur city in {$country}.</br>";
+echo $aboutMe;
